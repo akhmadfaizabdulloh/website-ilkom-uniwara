8ef3356ac9c17e37a33915651c3ee5c47751deb4
Akhmad Faiz Abdulloh <akhmadfaizabdulloh@gmail.com>
2023-04-16 01:58:54 +0700

    delete navbar-fixed

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,3 @@
-//Navbarr fixed
-window.onscroll = function(){
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-
-    if (window.pageYOffset > fixedNav){
-        header.classList.add('navbar-fixed')
-    } else {
-        header.classList.remove('navbar-fixed')
-    }
-
-};
-
 //Hamburger
 const hamburger = document.querySelector('#hamburger');
 const navMenu =document.querySelector('#nav-menu');
